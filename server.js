a777bf9aaecfef4269c83fb1620d7140ea9322cd
Matej Rifelj <matej@rifelj.com>
2022-06-26 20:44:09 +0200

    wip

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const express = require("express");
+import got from "got";
+import express from "express";
+import ffmpegPath from "ffmpeg-static";
+import ffmpeg from "fluent-ffmpeg";
+ffmpeg.setFfmpegPath(ffmpegPath);
+
 const app = express();
 const port = 80;
 
+console.log("??????????????", ffmpegPath);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // const proc = ffmpeg(got.stream("https://u3pm.com/a.mkv"))
+  const proc = ffmpeg("/app/content/a.mkv")
+    // setup event handlers
+    .on("filenames", function (filenames) {
+      console.log("screenshots are " + filenames.join(", "));
+    })
+    .on("end", function () {
+      console.log("screenshots were saved");
+    })
+    .on("error", function (err) {
+      console.log("an error happened: " + err.message);
+    })
+    // take 2 screenshots at predefined timemarks and size
+    .takeScreenshots({ count: 2, timemarks: ["00:00:02.000", "6"], width: "320" }, "/app/xxx");
+  res.send("Hello World and some...!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+export default () => {};
